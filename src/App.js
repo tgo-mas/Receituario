dddb904ecd922594f91a8c3bfe2264153e8cc2ba
tgo-mas <talmeidasf@gmail.com>
2023-11-16 11:14:19 -0300

    Adicionando navegação da lista para página da receita

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Root from './pages/Root/Root.jsx';
 import Introducao from './pages/Introducao/Introducao.jsx';
 import ListaReceitas from './pages/ListaReceitas/ListaReceitas.jsx';
+import Receita from './pages/Receita/Receita.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Root />}>
           <Route path="/" element={<Introducao />} />
           <Route path="/receitas" element={<ListaReceitas />} />
+          <Route path="/receita/:id" element={<Receita />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/ListaReceitas/ListaReceitas.jsx
+++ src/pages/ListaReceitas/ListaReceitas.jsx
@@ -4,8 +4,11 @@ import htmlIcon from "../../assets/icon _Code_.svg"
 import reactIcon from "../../assets/icon _react_.svg"
 import nextIcon from "../../assets/icon _nextjs_.svg"
 import "./ListaReceitas.css";
+import { useNavigate } from "react-router-dom";
 
 export default function ListaReceitas() {
+    const navigate = useNavigate();
+
     return (
         <main className="main-container mt-4 pt-3">
             <Container className="ms-4 p-4">
@@ -21,7 +24,7 @@ export default function ListaReceitas() {
                     </span> */}
                 </div>
                 <Container className="lista-container">
-                    {receitas.map((receita, index) => <span className="receita-card" key={index}><h4>{receita.titulo}</h4></span>)}
+                    {receitas.map((receita) => <span className="receita-card" onClick={() => navigate(`/receita/${receita.id}`)} key={receita.id}><h4>{receita.titulo}</h4></span>)}
                 </Container>
             </Container>
         </main>
